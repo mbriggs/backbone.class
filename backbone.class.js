d26324f702dca46876f27ab0edb88fb8010dff42
Matt Briggs <matt@mattbriggs.net>
2013-08-29 11:28:17 -0400

    add .define for singleton definitions

--- backbone.class.js
+++ backbone.class.js
@@ -1,6 +1,9 @@
 if(Backbone.Class) throw new Error("Something already exists at Backbone.Class!!")
 Backbone.Class = function(){ this.initialize.apply(this, arguments) };
 Backbone.Class.extend = Backbone.Model.extend;
-Backbone.Class.prototype.initialize = function(){};
 _.extend(Backbone.Class.prototype, Backbone.Events);
-
+Backbone.Class.prototype.initialize = function(){};
+Backbone.Class.define = function(definition){
+  var klass = this.extend(definition);
+  return new klass();
+}
